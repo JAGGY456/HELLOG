196a114362a67603089277ffb640d045b24cf956
JAGGY456 <113669433+JAGGY456@users.noreply.github.com>
2022-09-16 13:29:45 +0530

    Add files via upload

---
+++ jayesh.py
@@ -0,0 +1,7 @@
+#INTIALIZE ARRAY
+arr=[1,2,3,4,5];
+sum=0;
+#loop through the array to calculate sum of elements
+for i in range (0,len(arr)):
+    sum=sum+arr[i]
+ print=("sum of all the elementsof an array=",sum)
